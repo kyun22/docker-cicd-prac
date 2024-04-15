bac3bc9b31f052896eeeea01ab0aaf5e7f4078e9
shlee <shlee@rsupport.com>
2024-04-16 01:49:30 +0900

    postgres DB로 변경

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -12,10 +12,12 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework:spring-tx:6.1.5")
     // swagger
 //    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
 
     runtimeOnly("com.h2database:h2")
+    runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.mockk:mockk:1.13.10")
